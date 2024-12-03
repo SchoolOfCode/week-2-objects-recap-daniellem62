7f3a753e38cc3fca12fecd4952568f7118f77030
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-03 16:13:58 +0000

    Created new files, linked them and made objects

---
+++ main.js
@@ -0,0 +1,35 @@
+const myCat = {
+    color: "black",
+    name: "Tony",
+    lovesCuddles: true,
+  };
+
+const myHouse = {
+    numberOfStories: 2,
+    parking: false,
+    numberOfBedrooms: 2,
+    garden: true,
+    energyRating: "D",
+};
+
+const myCar = {
+    color: "red",
+    numberOfWheels: 4,
+    powerSteering: true,
+    numberOfSeats: 2,
+    fuelConsumption: "20mpg",
+};
+
+const myFavouriteFilm = {
+    filmTitle: "The Dark Knight",
+    released: 2008,
+    leadActor: "Christian Bale",
+};
+
+  //Create an object called myHouse:
+
+//My house has 2 stories, no parking, 2 bedrooms, a garden and an energy efficiency rating of D. done
+//Create an object called myCar:
+//My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per gallon.
+//Create an object called myFavouriteFilm:
+//My favourite film's title is The Dark Knight, it was released in 2008, and the lead actor is Christian Bale.
